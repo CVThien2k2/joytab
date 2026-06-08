c5b68b8186bdad1286dbdf5ea82712411b948ac0
ThienCV <caovanthien09102002@gmail.com>
2026-06-08 15:58:32 +0700

    feat(auth): SwitchAccountDto, xoá DTO exchange/refresh/logout

--- api/src/auth/dto/refresh.dto.ts
+++
@@ -1,8 +0,0 @@
-import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
-
-export class RefreshDto {
-  @IsString()
-  @IsNotEmpty()
-  @IsUUID()
-  accountId!: string;
-}
--- api/src/auth/dto/switch-account.dto.ts
+++ api/src/auth/dto/switch-account.dto.ts
@@ -1,8 +1,8 @@
 import { IsNotEmpty, IsString, IsUUID } from 'class-validator';
 
-export class LogoutDto {
+export class SwitchAccountDto {
   @IsString()
   @IsNotEmpty()
   @IsUUID()
-  accountId!: string;
+  userId!: string;
 }
